0f9aa15845c0d297a71aca03df8261409318238b
Florian_Sebulke <florian.sebulke@volkswagen.de>
2025-01-22 13:26:06 +0100

    feat: introduction Popup

--- src/components/QuizComponents/quizComponents/QuizIntroduciton.tsx
+++ src/components/QuizComponents/quizComponents/QuizIntroduciton.tsx
@@ -1,46 +1,46 @@
 import {
   BaseButton,
-  Card,
+  Popup,
 } from "/Users/vwbspk0/Desktop/VsCode/npm-packages/sebu-dev-react-lib";
+
 interface QuizIntroductionProps {
   handleOnClick: () => void;
 }
+
 export const QuizIntroduction = ({ handleOnClick }: QuizIntroductionProps) => {
   return (
-    <Card className="fixed bg-neutral-800 inset-1 flex items-center justify-center ">
-      {" "}
-      <h2 className="text-2xl font-bold text-cyan-500 mb-4">
-        Willkommen zum Quiz!
-      </h2>
-      <p className="text-gray-700 mb-4">
-        Teste dein Wissen in verschiedenen Schwierigkeitsgraden! Bevor wir
-        starten, hier sind die Spielregeln:
-      </p>
-      <ul className="list-disc list-inside mb-4 text-gray-600">
-        <li>
-          <strong>Easy:</strong> Du erhältst <strong>1 Punkt</strong>, wenn alle
-          richtigen Antworten ausgewählt wurden und keine falsche Antwort
-          markiert ist.
-        </li>
-        <li>
-          <strong>Medium:</strong> Maximal <strong>1 Punkt</strong>. Wähle eine
-          falsche Antwort, und du erhältst <strong>0,5 Punkte</strong>. Wählst
-          du zwei oder mehr falsche Antworten, bekommst du
-          <strong> 0 Punkte</strong>.
-        </li>
-        <li>
-          <strong>Hard:</strong> Maximal <strong>2 Punkte</strong>. Für jede
-          richtige Antwort gibt es <strong>+0,5 Punkte</strong>, aber jede
-          falsche Antwort zieht <strong>-1 Punkt</strong> ab. Wenn du mehr als
-          eine falsche Antwort auswählst, bekommst du <strong>0 Punkte</strong>.
-        </li>
-      </ul>
-      <p className="text-gray-700">
-        Überlege dir deine Antworten gut, und viel Erfolg beim Quiz! 😊
-      </p>
-      <div>
-        <BaseButton handleOnClick={handleOnClick}>Ok</BaseButton>
+    <Popup className="p-4 rounded-2xl justify-center bg-neutral-800/70 items-center shadow-lg flex ">
+      <div className="flex flex-col text-pretty">
+        <h2 className="text-2xl font-bold text-cyan-500 mb-4">Willkommen!</h2>
+        <p className="text-gray-300 mb-4">
+          <span className="hidden xl:inline">Teste dein Wissen! </span>
+          Hier die Regeln:
+        </p>
+        <ul className="list-none list-inside mb-4 text-neutral-300">
+          <li>
+            <strong className="text-amber-700">Easy:</strong> 1 Punkt für alle
+            richtigen Antworten, keine falsche markiert.
+          </li>
+          <li>
+            <strong className="text-amber-700">Medium:</strong> 1 Punkt max. 0,5
+            Punkte für 1 falsche Antwort, ab 2 falsch = 0 Punkte.
+          </li>
+          <li>
+            <strong className="text-amber-700">Hard:</strong> 2 Punkte max. Jede
+            richtige Antwort: +0,5 Punkte, jede falsche: -1 Punkt. Ab 2 falsche
+            = 0 Punkte.
+          </li>
+        </ul>
+        <p className="text-gray-300">Viel Erfolg beim Quiz! 😊</p>
+        <div className="flex justify-center pt-8">
+          <BaseButton
+            bgColor="bg-cyan-500 hover:bg-cyan-500/50"
+            handleOnClick={handleOnClick}
+          >
+            Bestätigen
+          </BaseButton>
+        </div>
       </div>
-    </Card>
+    </Popup>
   );
 };
